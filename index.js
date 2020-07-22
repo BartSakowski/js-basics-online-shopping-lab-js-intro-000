856d16e022f3dbac78d89beced2c87ac60f9f554
Bart Sakowski <bart.sakowski@gmail.com>
2020-07-22 21:40:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,12 +10,23 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+ cart.push( {itemName: item, itemPrice: Math.floor(Math.random() * 101)} );
+ return( `${item} has been added to your cart.`);
 }
 
 function viewCart() {
   // write your code here
+  if (cart.length === 0) {
+    return("Your shopping cart is empty.")
+  } else if (cart.length === 1) {
+    return(`In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`)
+  } else if (cart.length === 2) {
+    return (`In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}, and ${cart[1].itemName} at $${cart[1].itemPrice}.`)
+  } else {
+    return(`In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}, ${cart[1].itemName} at $${cart[1].itemPrice}, and ${cart[cart.length - 1].itemName} at $${cart[cart.length - 1].itemPrice}.`)
+  }
 }
+
 
 function total() {
   // write your code here
